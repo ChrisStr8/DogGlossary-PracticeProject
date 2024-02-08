f9ca9e8a477720e7e5be2b8fa226faf383dc6c27
Chris.Straight <chris.str8.systems@gmail.com>
2024-02-08 15:43:16 +1100

    part 2 complete

--- Dog Glossary/task/src/script.js
+++ Dog Glossary/task/src/script.js
@@ -1,14 +1,23 @@
-let btn = document.getElementById("button-random-dog");
+let rbtn = document.getElementById("button-random-dog");
+let bbtn = document.getElementById("button-show-breed");
+
+let binpt = document.getElementById("input-breed");
+
 let content = document.getElementById("content");
-let img = document.createElement("img");
-content.appendChild(img);
 
-btn.addEventListener('click', e => {
-    img.alt = 'dogo';
-    //console.log('test');
+rbtn.addEventListener('click', e => {
     fetch("https://dog.ceo/api/breeds/image/random").then()
-        .then((response) => response.json())
-        .then((json) => img.src = json.message)
-        .catch((e) => console.log(e.message));
-
+        .then(response => response.json())
+        .then(json => content.innerHTML = "<img src=\""+json.message+"\" alt=\"dogo\">")
+        .catch(e => console.log(e.message));
 });
+
+bbtn.addEventListener('click', e => {
+    let breed = binpt.value.toLowerCase();
+    fetch("https://dog.ceo/api/breed/"+breed+"/images/random")
+        .then(response => response.json())
+        .then(json => content.innerHTML = json.status === "success" ? "<img src=\"" + json.message + "\" alt=\"dogo\">" :
+            "<p>Breed not found!</p>")
+        .catch(e => console.log(e.message))
+});
+
